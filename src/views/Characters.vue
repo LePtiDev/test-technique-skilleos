fe29fe980529d4256a07e3040f557c9dcc917fe8
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-03-25 15:42:58 +0100

    [add] axios method for characters

--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -1,12 +1,31 @@
 <template>
     <div class="">
         <h2>Personnages</h2>
+        <pre>{{characters}}</pre>
     </div>
 </template>
 
 <script>
+
+    const axios = require("axios");
+
     export default {
-        name: "Character"
+        name: "Character",
+        data(){
+            return {
+                characters: {},
+                page: 1
+            }
+        },
+        mounted() {
+            this.getCharacters()
+        },
+        methods: {
+            async getCharacters(){
+                const { data } = await axios.get("https://rickandmortyapi.com/api/character/?page=" + this.page);
+                this.characters = data;
+            }
+        }
     }
 </script>
 
