b27aed4ca8ad7ae15e0e840e4771807802cac4f3
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-03-25 16:01:04 +0100

    [add] axios method for episodes

--- src/views/Episodes.vue
+++ src/views/Episodes.vue
@@ -1,13 +1,32 @@
 <template>
     <div class="">
         <h2>Episodes</h2>
+        <pre>{{episodes}}</pre>
     </div>
 </template>
 
 <script>
-export default {
-name: "Episodes"
-}
+
+    const axios = require("axios");
+
+    export default {
+        name: "Episodes",
+        data(){
+            return {
+                episodes: {},
+                page: 1
+            }
+        },
+        mounted() {
+            this.getEpisodes()
+        },
+        methods: {
+            async getEpisodes(){
+                const { data } = await axios.get("https://rickandmortyapi.com/api/episode/?page=" + this.page);
+                this.episodes = data;
+            }
+        }
+    }
 </script>
 
 <style scoped>
