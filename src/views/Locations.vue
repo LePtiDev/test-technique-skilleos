685806f7b2ff25f6dc1f4d45a580e296747414b8
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-03-25 16:07:39 +0100

    [add] axios method for locations

--- src/views/Locations.vue
+++ src/views/Locations.vue
@@ -1,13 +1,32 @@
 <template>
     <div class="">
         <h2>Localisations</h2>
+        <pre>{{locations}}</pre>
     </div>
 </template>
 
 <script>
-export default {
-name: "Locations"
-}
+
+    const axios = require("axios");
+
+    export default {
+        name: "Locations",
+        data(){
+            return {
+                locations: {},
+                page: 1
+            }
+        },
+        mounted() {
+            this.getLocations()
+        },
+        methods: {
+            async getLocations(){
+                const { data } = await axios.get("https://rickandmortyapi.com/api/episode/?page=" + this.page);
+                this.locations = data;
+            }
+        }
+    }
 </script>
 
 <style scoped>
