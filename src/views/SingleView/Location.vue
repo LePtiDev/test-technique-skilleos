599903844e44d06e2be7d0afb6f4358b424b9ddf
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-03-28 19:31:49 +0200

    [add] single view location design

--- src/views/SingleView/Location.vue
+++ src/views/SingleView/Location.vue
@@ -1,16 +1,22 @@
 <template>
-    <div class="">
-        <h2>Localisation: {{$route.params.id}}</h2>
-        <pre>{{location}}</pre>
+    <div class="container-pages">
+        <div class="card-location">
+            <h3>{{location.name}}</h3>
+            <p><span class="section">Date </span><span class="badge rounded-pill bg-primary">{{location.air_date}}</span></p>
+            <p><span class="section">Gender </span><span class="badge rounded-pill bg-primary">{{location.episode}}</span></p>
+        </div>
+        <CharacterList :link="location.characters"/>
     </div>
 </template>
 
 <script>
 
+    import CharacterList from "@/components/characters/CharacterList";
     const axios = require("axios");
 
     export default {
         name: "Location",
+        components: {CharacterList},
         data(){
             return {
                 location: {}
@@ -28,6 +34,31 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    .card-location{
+        width: 100%;
+        border-radius: 10px;
+        border: solid 1px grey;
+        padding: 15px;
+        display: flex;
+        position: relative;
+        flex-direction: column;
+        margin: 20px 0;
+    }
 
+    .container-location-text{
+        width: auto;
+        margin-left: 20px;
+
+        h3{
+            font-size: 30px;
+        }
+
+        p{
+            .section{
+                color: gray;
+                margin-right: 10px;
+            }
+        }
+    }
 </style>
