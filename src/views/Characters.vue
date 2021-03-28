8f8335394c6faddc4fd1ee4e1e730396f94c8e81
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-03-28 19:33:10 +0200

    [add] add navigation page for characters

--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -1,31 +1,43 @@
 <template>
     <div class="container-pages">
-        <h2>Personnages</h2>
-        <CharacterList :characters="characters.results"/>
+        <h2>Characters</h2>
+        <PageNavigation :page="page" :info="characters.info" @next-page="nextPage" @previous-page="previousPage"/>
+        <CharacterList :key="page" :characters="characters.results"/>
     </div>
 </template>
 
 <script>
 
     import CharacterList from "@/components/characters/CharacterList";
+    import PageNavigation from "@/components/PageNavigation";
     const axios = require("axios");
 
     export default {
         name: "Character",
-        components: {CharacterList},
+        components: {PageNavigation, CharacterList},
         data(){
             return {
-                characters: {},
+                characters: [],
                 page: 1
             }
         },
-        mounted() {
+        beforeMount() {
             this.getCharacters()
         },
         methods: {
             async getCharacters(){
                 const { data } = await axios.get("https://rickandmortyapi.com/api/character/?page=" + this.page);
                 this.characters = data;
+            },
+
+            nextPage(){
+                this.page ++
+                this.getCharacters()
+            },
+
+            previousPage(){
+                this.page --
+                this.getCharacters()
             }
         }
     }
