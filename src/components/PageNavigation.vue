1c76985772590275394275fd118358803e977d19
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-03-28 19:32:30 +0200

    [add] page navigation components

---
+++ src/components/PageNavigation.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+            <li class="page-item" @click="previousPage" v-if="page > 1"><a class="page-link" href="#">Previous</a></li>
+            <li class="page-item btn-success"><a class="page-link" href="#">{{page}}</a></li>
+            <li class="page-item" @click="nextPage" v-if="page < info.pages"><a class="page-link" href="#">Next</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "PageNavigation",
+        props: {
+            page: Number,
+            info: Object
+        },
+        methods: {
+            nextPage(){
+                this.$emit('next-page')
+            },
+
+            previousPage(){
+                this.$emit('previous-page')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
