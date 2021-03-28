3a5b18793685f4b33be7255d187a44f139ba4c52
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-03-28 19:31:03 +0200

    [add] locations list and list item

---
+++ src/components/Localisations/LocalisationList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container-card-location">
+        <LocalisationListItem v-for="(location, index) in locations" :key="index" :location="location"/>
+    </div>
+</template>
+
+<script>
+    import LocalisationListItem from "@/components/Localisations/LocalisationListItem";
+    export default {
+        name: "localisationList",
+        components: {
+            LocalisationListItem
+        },
+        props: {
+            locations: Array,
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .container-card-location{
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        flex-wrap: wrap;
+    }
+</style>
---
+++ src/components/Localisations/LocalisationListItem.vue
@@ -0,0 +1,46 @@
+<template>
+    <router-link :to="{ name: 'Location', params: { id: location.id }}">
+        <div class="card-episodes">
+            <h3>{{location.name}}</h3>
+            <div class="container-date-episode">
+                <p>{{location.type}}</p>
+                <p>{{location.dimension}}</p>
+            </div>
+        </div>
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: "LocalisationListItem",
+    props: {
+        location: Object,
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .card-episodes{
+        width: 450px;
+        background-color: white;
+        margin: 15px 30px;
+        border-radius: 10px;
+        padding: 10px;
+        -webkit-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.26);
+        -moz-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.26);
+        box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.26);
+
+        .container-date-episode{
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+
+        &:hover{
+            -webkit-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.26);
+            -moz-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.26);
+            box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.26);
+
+        }
+    }
+</style>
