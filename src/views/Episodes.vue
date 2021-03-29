69a01764e65f8f17d4d02788bcf131b0eec88059
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-03-29 20:58:31 +0200

    [add] Navigation page on the episodes

--- src/views/Episodes.vue
+++ src/views/Episodes.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="container-pages">
         <h2>Episodes</h2>
+        <PageNavigation :info="episodes.info" :page="page" @next-page="nextPage" @previous-page="previousPage" />
         <EpisodeList :episodes="episodes.results"/>
     </div>
 </template>
 
 <script>
 
+    import PageNavigation from "@/components/PageNavigation";
     const axios = require("axios");
 
     //components
@@ -14,20 +16,30 @@
 
     export default {
         name: "Episodes",
-        components: {EpisodeList},
+        components: {PageNavigation, EpisodeList},
         data(){
             return {
                 episodes: {},
                 page: 1
             }
         },
-        mounted() {
+        beforeMount() {
             this.getEpisodes()
         },
         methods: {
             async getEpisodes(){
                 const { data } = await axios.get("https://rickandmortyapi.com/api/episode/?page=" + this.page);
                 this.episodes = data;
+            },
+
+            nextPage(){
+                this.page ++
+                this.getEpisodes()
+            },
+
+            previousPage(){
+                this.page --
+                this.getEpisodes()
             }
         }
     }
