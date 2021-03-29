d72cee6b7fd77baea6b8b71787244583e6e487d1
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-03-29 21:01:01 +0200

    [add] design and show 4 characters

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,53 @@
 <template>
-  <div class="home">
-    <h2>Home</h2>
-  </div>
+    <div class="home container-pages">
+    <h2>Welcome</h2>
+    <p>
+      This documentation will help you get familiar with the characters, location and episodes of the Rick and Morty.
+    </p>
+    <h2 class="mt-5">Characters</h2>
+    <div class="container-show">
+        <CharacterList :characters="characters"/>
+        <router-link class="btn btn-primary" :to="{ name: 'Characters'}">View all</router-link>
+    </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'Home',
-}
+    import CharacterList from "@/components/characters/CharacterList";
+    const axios = require("axios");
+    export default {
+      name: 'Home',
+        components: {
+          CharacterList
+        },
+        data(){
+            return {
+                characters: [],
+                page: 1
+            }
+        },
+        beforeMount() {
+            this.getCharacters()
+        },
+        methods: {
+            async getCharacters() {
+                const {data} = await axios.get("https://rickandmortyapi.com/api/character/[1,2,3,4]");
+                this.characters = data;
+            }
+        }
+    }
 </script>
+
+<style scoped lang="scss">
+    .container-show{
+        height: auto;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+        a{
+            margin-top: 20px;
+            margin-left: 30px;
+        }
+    }
+</style>
